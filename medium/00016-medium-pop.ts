ab2c6f7797c72e679a2d1d48894d4a68a9a2ceca
Gerhardus <hardus.cronje@gmail.com>
2025-08-05 20:39:49 +0200

    feat: implemented pop

--- medium/00016-medium-pop.ts
+++ medium/00016-medium-pop.ts
@@ -26,7 +26,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer K, any] ? K : [];
+
+type Item = Pop<[1,2,3]>;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../type-challenges'
